28ac3e82b45b8750bb1732620fa2c2ad0780c072
Ivan <ivaanpavlovic@gmail.com>
2025-04-30 23:56:09 +0100

    modularize weather data

---
+++ Ivan/weather_data.js
@@ -0,0 +1,24 @@
+const weatherData = {
+  "Dublin City Centre": 14.5,
+  "Temple Bar": 14.8,
+  "Rathmines": 15.2,
+  "Dundrum": 16.1,
+  "Ballsbridge": 15.6,
+  "Donnybrook": 15.0,
+  "Clontarf": 14.9,
+  "Drumcondra": 15.3,
+  "Phibsborough": 15.1,
+  "Howth": 13.8,
+  "Sandymount": 15.4,
+  "Malahide": 14.2,
+  "Blanchardstown": 16.0,
+  "Tallaght": 16.3,
+  "Lucan": 15.7,
+  "Swords": 14.7,
+  "Ballyfermot": 15.0,
+  "Cabra": 15.1,
+  "Coolock": 14.6,
+  "Artane": 14.5
+};
+
+module.exports = weatherData;
