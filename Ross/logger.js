bf2a4b4c9205ef739250f89d8c54ff450d152eec
gibneyross <x23233681@student.ncirl.ie>
2025-05-08 23:03:50 +0100

    Update logger.js with commentary

--- Ross/logger.js
+++ Ross/logger.js
@@ -1,12 +1,14 @@
-const winston = require('winston');
-const logger = winston.createLogger({
-	level: 'info',
+const winston = require('winston'); //winston logging library import
+const logger = winston.createLogger({ //logger instance
+	level: 'info', //minimum level
+	//log format including timestamp
 	format: winston.format.combine(
 		winston.format.timestamp(),
 		winston.format.printf(
 			({ timestamp, level, message})=> `[${timestamp}] ${level.toUpperCase()}: ${message}`
 		)
 	),
+	//Output logs to console and a signal.log folder under logs folder
 	transports: [
 		new winston.transports.Console(),
 		new winston.transports.File({ filename: 'logs/signal.log'})
